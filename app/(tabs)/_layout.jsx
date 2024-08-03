2410a8cd139ee446f5049ac4167b9c9c846851c0
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-08-04 01:04:12 +0530

    updated

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const _layout = () => {
+const TabIcon = ({ name, color, focused }) => {
   return (
-    <View>
-      <Text>_layout</Text>
+    <View style={{ alignItems: 'center' }}>
+      <Icon
+        name={name} // 'home' for the home icon
+        size={24}
+        color={focused ? color : 'gray'} // Change color based on focus
+      />
+      <Text style={{ color: focused ? color : 'gray' }}>{name}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default _layout
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen
+          name='home'
+          options={{
+            title: "",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                name='home' 
+                color={color}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const [query] = () => {
+const Search = () => {
   return (
     <View>
-      <Text>[query]</Text>
+      <Text>Search</Text>
     </View>
   )
 }
 
-export default [query]
+export default Search
